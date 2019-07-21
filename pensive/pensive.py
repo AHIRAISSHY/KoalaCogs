81737ba4685e5f9ac612d9b4010a9921c6a920b9
Angel Hiraldo <ahiraldo41@gmail.com>
2019-07-21 13:41:46 -0400

    [Pensive] removed the webhooksay command will fix
    later

--- pensive/pensive.py
+++ pensive/pensive.py
@@ -25,28 +25,3 @@ class Pensive(commands.Cog):
         if ctx.channel.permissions_for(ctx.guild).manage_messages:
             await ctx.message.delete()
         await ctx.send(msg)
-
-    @commands.command(hidden=True, aliases=['hooksay'])
-    @commands.bot_has_permissions(manage_webhooks=True)
-    async def webhooksay(self, ctx, member: Optional[discord.Member], *, msg: str):
-        """
-           Beep boop boop beep i\'m a robot
-        """
-        # https://github.com/TrustyJAID/Trusty-cogs/blob/master/trustybot/trustybot.py#L64
-        if member is None:
-            member = ctx.me
-        if ctx.channel.permissions_for(ctx.me).manage_messages:
-            await ctx.message.delete()
-        guild = ctx.guild
-        webhook = None
-        for hook in await ctx.channel.webhooks():
-            if hook.name == guild.me.name:
-                webhook = hook
-        if webhook is None:
-            webhook = await ctx.channel.create_webhook(name=guild.me.name)
-        avatar = member.avatar_url_as(format="png")
-        msg = msg.replace("@everyone", "everyone").replace("@here", "here")
-        for mention in ctx.message.mentions:
-            msg = msg.replace(mention.mention, mention.display_name)
-        # Apparently webhooks have @everyone permissions
-        await webhook.send(msg, username=member.display_name, avatar_url=avatar)
