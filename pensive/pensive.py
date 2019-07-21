4f5cbf5e6c5ff63617cf8efde00818db9a6d0ce8
Angel Hiraldo <ahiraldo41@gmail.com>
2019-07-21 11:57:39 -0400

    Update pensive.py

--- pensive/pensive.py
+++ pensive/pensive.py
@@ -43,10 +43,10 @@ class Pensive(commands.Cog):
             if hook.name == guild.me.name:
                 webhook = hook
         if webhook is None:
-            webhook = await ctx.channel.create_webhook(name=guild.name.me)
+            webhook = await ctx.channel.create_webhook(name=guild.me.name)
         avatar = member.avatar_url_as(format="png")
         msg = msg.replace("@everyone", "everyone").replace("@here", "here")
         for mention in ctx.message.mentions:
             msg = msg.replace(mention.mention, mention.display_name)
-
+        # Apparently webhooks have @everyone permissions
         await webhook.send(msg, username=member.display_name, avatar_url=avatar)
