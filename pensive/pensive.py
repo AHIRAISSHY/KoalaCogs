0b989ed3951060847fa455671153757ef5084058
Angel Hiraldo <ahiraldo41@gmail.com>
2019-07-20 22:30:11 -0400

    [Pensive] New Command

--- pensive/pensive.py
+++ pensive/pensive.py
@@ -1,5 +1,6 @@
 import discord
 from redbot.core import commands
+from typing import Optional
 
 
 class Pensive(commands.Cog):
@@ -9,3 +10,8 @@ class Pensive(commands.Cog):
     async def support(self, ctx):
         """Support Server"""
         await ctx.send("https://discord.gg/xGEMMmy")
+
+    @commands.command(hidden=True)
+    async def say(self, ctx, *, msg: str):
+        """Make the bot repeat things"""
+        await ctx.send(msg)
