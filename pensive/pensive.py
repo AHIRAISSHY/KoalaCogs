22c722323ff2df338d35f908810a4f545bc464f9
Angel Hiraldo <ahiraldo41@gmail.com>
2019-07-21 12:43:31 -0400

    Update pensive.py

--- pensive/pensive.py
+++ pensive/pensive.py
@@ -45,7 +45,7 @@ class Pensive(commands.Cog):
         if webhook is None:
             webhook = await ctx.channel.create_webhook(name=guild.me.name)
         avatar = member.avatar_url_as(format="png")
-        msg = msg.replace("@everyone", "everyone").replace("@here", "here")
+        msg = bot.send_filtered(ctx, content='@everyone')
         for mention in ctx.message.mentions:
             msg = msg.replace(mention.mention, mention.display_name)
         await webhook.send(msg, username=member.display_name, avatar_url=avatar)
