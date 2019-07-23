d79576b1d34a88fc4ae6c578d137296e8a9c413e
Angel Hiraldo <ahiraldo41@gmail.com>
2019-07-23 17:41:45 -0400

    Update pensive.py

--- pensive/pensive.py
+++ pensive/pensive.py
@@ -35,5 +35,5 @@ class Pensive(commands.Cog):
 
     @commands.command()
     async def pensivespam(self, ctx):
-        msg =  '<:pensivecowboy:592510037793177619>' * 100
+        msg = '<:pensivecowboy:592510037793177619>' * 100
         await ctx.send(msg)
